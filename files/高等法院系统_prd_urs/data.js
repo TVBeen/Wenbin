3c9749de95b844bb942e8e689a2cc2c434d36ea2
TVBeen <Wenbin@TianWenbindeMacBook-Pro.local>
2017-12-07 16:47:29 +0800

    我的简历.2017/12/08

---
+++ files/高等法院系统_prd_urs/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,w,E,w,F,G,H,null,I,J,K,L,M,N,O,J),P,_(Q,_()),R,_(S,_(T,U,V,[_(T,W,X,g,Y,[_(Z,ba,T,bb,bc,_(bd,be,bf,_(bg,bh,bi,bj,bk,[]),bl,g),bm,bn)])])),bo,_(bp,[])),bq,_(),br,_());}; 
+var b="url",c="高等法院系统_prd_urs.html",d="generationDate",e=new Date(1512635759333.36),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="page",l="packageId",m="a7a819ad723a4879887d2f940c660c64",n="type",o="Axure:Page",p="name",q="高等法院系统 PRD/URS",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="near",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="a",R="interactionMap",S="onLoad",T="description",U="页面载入时",V="cases",W="Case 1",X="isNewIfGroup",Y="actions",Z="action",ba="linkWindow",bb="在 当前窗口 打开 https://pan.baidu.com/s/1o8qSIsa",bc="target",bd="targetType",be="webUrl",bf="urlLiteral",bg="exprType",bh="stringLiteral",bi="value",bj="https://pan.baidu.com/s/1o8qSIsa",bk="stos",bl="includeVariables",bm="linkType",bn="current",bo="diagram",bp="objects",bq="masters",br="objectPaths";
+return _creator();
+})());
